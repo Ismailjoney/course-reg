2a5baee8d2878200ffa028e6e528ccb26564871a
Ismailjoney <mohammadismailjoney@gmail.com>
2024-02-29 19:27:53 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/hedaer/Header'
+
+function App() {
+
+
+  return (
+    <>
+      <Header></Header>
+       
+    </>
+  )
+}
+
+export default App
